a6c1af7dcac2c57984dee391a85ae85ec3bfcd1f
Gilvan <gilvanritter@gmail.com>
2018-03-21 14:56:44 -0300

    slave must be READ_ONLY

--- docker/database/config/mysql_connector.sh
+++ docker/database/config/mysql_connector.sh
@@ -12,9 +12,9 @@ mysql --host database -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "stop slave;";
 mysql --host database -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "reset slave all;";
 
 
-echo "stopping slave in MASTER MYSQL"
-mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e "stop slave;";
-mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e "reset slave all;";
+#echo "stopping slave in MASTER MYSQL"
+#mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e "stop slave;";
+#mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e "reset slave all;";
 
 
 echo "creating replication user in MASTER MYSQL"
@@ -23,10 +23,10 @@ mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e "
 mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e "flush privileges;"
 
 
-echo "creating replication user in SLAVE MYSQL"
-mysql --host $MYSQL_SLAVE_ADDRESS -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "create user '$MYSQL_REPLICATION_USER'@'%';"
-mysql --host $MYSQL_SLAVE_ADDRESS -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "grant replication slave on *.* to '$MYSQL_REPLICATION_USER'@'%' identified by '$MYSQL_REPLICATION_PASSWORD';"
-mysql --host $MYSQL_SLAVE_ADDRESS -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "flush privileges;"
+#echo "creating replication user in SLAVE MYSQL"
+#mysql --host $MYSQL_SLAVE_ADDRESS -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "create user '$MYSQL_REPLICATION_USER'@'%';"
+#mysql --host $MYSQL_SLAVE_ADDRESS -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "grant replication slave on *.* to '$MYSQL_REPLICATION_USER'@'%' identified by '$MYSQL_REPLICATION_PASSWORD';"
+#mysql --host $MYSQL_SLAVE_ADDRESS -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "flush privileges;"
 
 
 echo "getting MASTER MYSQL config"
@@ -34,17 +34,17 @@ Master_Position="$(mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROO
 Master_File="$(mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -e 'show master status \G' | grep File | sed -n -e 's/^.*: //p')"
 
 
-echo "getting SLAVE MYSQL config"
-Slave_Position="$(mysql --host database -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -e 'show master status \G' | grep Position | grep -o '[0-9]*')"
-Slave_File="$(mysql --host database -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -e 'show master status \G' | grep File | sed -n -e 's/^.*: //p')"
+#echo "getting SLAVE MYSQL config"
+#Slave_Position="$(mysql --host database -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -e 'show master status \G' | grep Position | grep -o '[0-9]*')"
+#Slave_File="$(mysql --host database -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -e 'show master status \G' | grep File | sed -n -e 's/^.*: //p')"
 
 
 echo "set SLAVE to upstream MASTER"
 mysql --host database -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "change master to master_host='$MYSQL_MASTER_ADDRESS',master_user='$MYSQL_REPLICATION_USER',master_password='$MYSQL_REPLICATION_PASSWORD',master_log_file='$Master_File',master_log_pos=$Master_Position;"
 
 
-echo "set MASTER to upstream SLAVE"
-mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e"change master to master_host='$MYSQL_SLAVE_ADDRESS',master_user='$MYSQL_REPLICATION_USER',master_password='$MYSQL_REPLICATION_PASSWORD',master_log_file='$Slave_File',master_log_pos=$Slave_Position;"
+#echo "set MASTER to upstream SLAVE"
+#mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e"change master to master_host='$MYSQL_SLAVE_ADDRESS',master_user='$MYSQL_REPLICATION_USER',master_password='$MYSQL_REPLICATION_PASSWORD',master_log_file='$Slave_File',master_log_pos=$Slave_Position;"
 
 
 echo "start sync: MASTER to SLAVE"
@@ -52,9 +52,9 @@ mysql --host database -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -AN -e "start slave;"
 mysql --host database -uroot -p$MYSQL_SLAVE_ROOT_PASSWORD -e "show slave status \G;"
 
 
-echo "start sync: SLAVE to MASTER"
-mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e "start slave;"
-mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -e "show slave status \G;"
+#echo "start sync: SLAVE to MASTER"
+#mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -AN -e "start slave;"
+#mysql --host $MYSQL_MASTER_ADDRESS -uroot -p$MYSQL_MASTER_ROOT_PASSWORD -e "show slave status \G;"
 
 
 echo "mysql fine tuning and extra conf"
